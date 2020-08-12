8ea40782f6f6eb4455e78f25c281a5ff1239a9c1
Zaitsev Alexey <a.zaitsev@connectome.ai>
2020-08-12 14:05:44 +0300

    Initial Commit

---
+++ Lesson1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson1
+//
+//  Created by Zaitsev Aleksey on 12.08.2020.
+//  Copyright © 2020 Zaitsev Aleksey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
